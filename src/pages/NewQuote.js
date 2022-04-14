38d33e1141ede36a1443b7ca90dfa75cb5a1ba3a
Amr Mohsen <55943011+Amr-mohsen64@users.noreply.github.com>
2022-04-14 12:11:52 +0200

    inital commit

---
+++ src/pages/NewQuote.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const NewQuote = () => {
+    return (
+        <h1>NewQuote page</h1>
+
+    )
+}
+
+export default NewQuote
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const QuoteDetail = () => {
+    return (
+        <h1>QuoteDetail page</h1>
+
+    )
+}
+
+export default QuoteDetail
