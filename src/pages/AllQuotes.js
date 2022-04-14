e5a2bcd704d4e735ee3dc9b3d4d92f8847c20a0a
Amr Mohsen <55943011+Amr-mohsen64@users.noreply.github.com>
2022-04-14 12:42:58 +0200

    Added dumy data in all qoutes

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import QuoteList from '../components/quotes/QuoteList'
+
+const DUMMY_QUOTES = [
+    { id: 'q1', author: "max", text: "laring react is fun" },
+    { id: 'q2', author: "amt", text: "laring react is grate" },
+]
 
 const AllQuotes = () => {
     return (
-        <h1>All quotes page</h1>
+        <QuoteList quotes={DUMMY_QUOTES}/>
     )
 }
 
